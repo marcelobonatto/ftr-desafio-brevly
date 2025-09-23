bea76a8d318228da162f60000bfa7205e4029801
Marcelo Bonatto <marcelobonatto@gmail.com>
2025-09-23 11:17:09 -0300

    fix: fix NotFoundScreen design

--- web/src/components/not-found-screen.tsx
+++ web/src/components/not-found-screen.tsx
@@ -1,3 +1,17 @@
 export function NotFoundScreen() {
-  return <>Não encontrado</>;
+  return (
+    <>
+      <img src="/img/404.svg" alt="Brev.ly" className="w-48 h-20 mb-4" />
+      <h1 className="my-text-xl mb-2">Link não encontrado</h1>
+      <p className="mb-1 text-center max-w-[500px] mx-auto">
+        O link que você está tentando acessar não existe, foi removido ou é uma URL inválida.
+        <span className="block md:inline">
+          {" "}Saiba mais em{" "}
+          <a href={import.meta.env.VITE_FRONTEND_URL} className="underline text-blue-800 hover:text-blue-900 hover:font-bold">
+            brev.ly
+          </a>.
+        </span>
+      </p>
+    </>
+  )
 }
